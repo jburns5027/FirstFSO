d0f6368ee0b618b160d50191a1b507db6c79415c
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-13 14:24:02 -0500

    added vitamin loop

--- project2.js
+++ project2.js
@@ -1,1 +1,35 @@
-alert("JavaScript works!");
+// If it's cold, then I have to layer up.  If it's too cold, I'll run inside.
+
+var coldRun = ("Running in the cold!!!")
+
+if(coldRun === true){
+	console.log ("I have to Layer Up!!! ");{
+	
+}	if (body is warming as I run === true);
+		
+		console.log ("I'll keep running!!!");
+		} else {
+		console.log ("I's way to cold to run outside");
+		};
+} else {
+	console.log ("Looks like it's indoor running today.");
+
+};
+
+// my weightloss starts with a daily vitamin.
+
+var pillsLeftInBottle = 100;
+
+while (pillsLeftInBottle > 0){
+	console.log (pillsLeftInBottle +  " 'one-A-day' Vitamins left in the bottle.");
+	
+	pillsLeftInBottle--;
+};
+
+console.log (" Time to make a vitamin run!!!");
+
+
+
+
+
+		
