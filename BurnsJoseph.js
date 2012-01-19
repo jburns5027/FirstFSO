75a3a3cf8835d5227d06cab477f086f4fdb01428
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-19 14:45:12 -0500

    c25k progress added

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -33,6 +33,23 @@ if(beginWorkout === true);
 
 then = console.log (Nike GPS work starts to track progress);
 // Nike GPS progress code
+// Now, on to c25k app.
+ 	if(doneWithNike);
+ 
+ 	then(minimizeApp);
+ 
+ console.log("Time to add in c25k app, with Nike");
+ 	
+ 	if(openApp + C25K === True);
+ 
+ 	then(startWorkout);
+ 
+ Console.log("Time to start walking to the Gym");
+ 
+ 	if(walk + gym);
+ 	while(warmup);
+ 
+ then( console.log("RUN!!!"));
 
  
  
