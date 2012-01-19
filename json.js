7dfdd99c8032a997dbe61ecb52180023137d850f
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-19 15:41:29 -0500

    adding json

---
+++ json.js
@@ -0,0 +1,14 @@
+// My first atempt ad addding json to my script.
+
+var json ={
+	"fitness apps": {
+		"Nike GPS": {
+		"developer": "Nike",
+		"cost": "$1.99"
+	},
+	"c25k training app": {
+		"Developer" Lucius Kwok & Kasten searles",
+		 ""cost": "$1.99"
+		
+		
+	
