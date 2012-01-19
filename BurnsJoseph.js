eadbbc38521046558d256c2f16db34920bed3237
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-19 14:44:01 -0500

    Nike GPS progress added

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -1,1 +1,46 @@
-alert("JavaScript works!");
+
+var nikeGPS,
+	C25K,
+	inside,
+	gym;
+	
+function openApp,
+		 minimizeApp,
+		 startWorkout,
+		 chooseLocation,
+		 beginWorkout,
+		 doneWithNike,
+		 walk,
+		 warmup,
+		 RUN,
+		 chooseApp;
+// Mixing running indoors,and using technology is a process.
+
+	if(openApp = nikeGPs === True) {
+console.log ("The Process Begins");
+
+	then (startWorkout)
+
+}
+console.log ( Where am I working out today?);
+
+	if(inside),
+
+	then(chooseLocation);
+console.log(Treadmill it is.);
+
+if(beginWorkout === true);
+
+then = console.log (Nike GPS work starts to track progress);
+// Nike GPS progress code
+
+ 
+ 
+ 
+ 
+
+
+
+
+
+			 
