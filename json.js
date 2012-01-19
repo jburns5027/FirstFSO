4461c628bb9a08a05e15e7fd555c8e6caf7961da
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-19 15:56:44 -0500

    json 2

--- json.js
+++ json.js
@@ -17,6 +17,42 @@ for (var key in json.fitness apps) {
 	var fitness app = json.fitness apps[key]
 	
 };
+
+var json2 = {
+	"fitness apps": [
+		{ 
+			"Name": "Nike GPS",
+			"Dev,": "Nike",
+			"cost": "$1.99"
+		},
+		{
+			"Name": "c25k",
+			"Dev,": "Lucius Kwok & Kasten searles",
+			"Cost": "$1.99"
+			}
+			
+		]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 		
 		
 	
