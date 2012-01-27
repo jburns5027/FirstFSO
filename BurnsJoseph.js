fdfde3dc2deb02b8bf4ea79a8142fb2549175376
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-26 20:35:28 -0500

    third week function, but no flow chart

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -46,6 +46,8 @@ while (pillsLeftInBottle > 0){
 
 console.log (" Time to make a vitamin run!!!");
 
+/
+
 // I listed the 3 main, mojor workouts I enjoy doing.  If you don't enjoy a workout, you won't do it.
 
 var atwo = ["Upper Body", "Core", "Lower Body"];
@@ -98,5 +100,62 @@ for(var locationNumber = 0; locationNumber < workoutLocation.length; locationNum
  	for (var locationNumber = 0; locationNumber < workoutLocation.length; 		
  		locationNumber++) {
  		workoutAtOneLocation(locationNumber);
+ 		
+// Third Week, This week, we had no flow chart.  Threw me off a bit.
+
+
+var nikeGPS,
+	C25K,
+	inside,
+	gym;
+	
+function openApp,
+		 minimizeApp,
+		 startWorkout,
+		 chooseLocation,
+		 beginWorkout,
+		 doneWithNike,
+		 walk,
+		 warmup,
+		 RUN,
+		 chooseApp;
+// Mixing running indoors,and using technology is a process.
+
+	if(openApp = nikeGPs === True) {
+console.log ("The Process Begins");
+
+	then (startWorkout)
+
+}
+console.log ( Where am I working out today?);
+
+	if(inside),
+
+	then(chooseLocation);
+console.log(Treadmill it is.);
+
+if(beginWorkout === true);
+
+then = console.log (Nike GPS work starts to track progress);
+// Nike GPS progress code
+// Now, on to c25k app.
+ 	if(doneWithNike);
+ 
+ 	then(minimizeApp);
+ 
+ console.log("Time to add in c25k app, with Nike");
+ 	
+ 	if(openApp + C25K === True);
+ 
+ 	then(startWorkout);
+ 
+ Console.log("Time to start walking to the Gym");
+ 
+ 	if(walk + gym);
+ 	while(warmup);
+ 
+ then( console.log("RUN!!!"));
+ 
+ 
  		
  		
