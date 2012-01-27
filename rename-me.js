c8c0dc9f221e45f3c22df73439e9d69f63966a72
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-26 20:28:50 -0500

    First Week

---
+++ rename-me.js
@@ -0,0 +1,16 @@
+alert("JavaScript works!");
+
+// First Function from Week 1
+
+var coldRun = ("Running in the cold!!!")
+
+if(coldRun === true){
+	console.log ("I have to Layer Up!!! ");{
+	
+}	if = (body is warming as I run === true);
+		
+		console.log ("I'll keep running!!!");
+		} else {
+		console.log ("I's way to cold to run outside");{
+		};
+} 
