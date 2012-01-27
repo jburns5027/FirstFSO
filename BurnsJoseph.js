2bbfc45e0f78a2e38ce8b63b9c6680b118759a09
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-01-26 20:33:08 -0500

    Second Week, Not sure why it's purple

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -1,74 +1,102 @@
+alert("JavaScript works!");
 
-var nikeGPS,
-	C25K,
-	inside,
-	gym;
+// First Function from Week 1
+
+var coldRun = ("Running in the cold!!!")
+
+if(coldRun === true){
+	console.log ("I have to Layer Up!!! ");{
 	
-function openApp,
-		 minimizeApp,
-		 startWorkout,
-		 chooseLocation,
-		 beginWorkout,
-		 doneWithNike,
-		 walk,
-		 warmup,
-		 RUN,
-		 chooseApp;
-// Mixing running indoors,and using technology is a process.
+}	if = (body is warming as I run === true);
+		
+		console.log ("I'll keep running!!!");
+		} else {
+		console.log ("I's way to cold to run outside");{
+		};
+} 
 
-	if(openApp = nikeGPs === True) {
-console.log ("The Process Begins");
+// Week 2 with loops, arrays, boolean logic.
 
-	then (startWorkout)
+/ If it's cold, then I have to layer up.  If it's too cold, I'll run inside.
 
-}
-console.log ( Where am I working out today?);
+var coldRun = ("Running in the cold!!!")
 
-	if(inside),
-
-	then(chooseLocation);
-console.log(Treadmill it is.);
-
-if(beginWorkout === true);
-
-then = console.log (Nike GPS work starts to track progress);
-// Nike GPS progress code
-// Now, on to c25k app.
- 	if(doneWithNike);
- 
- 	then(minimizeApp);
- 
- console.log("Time to add in c25k app, with Nike");
- 	
- 	if(openApp + C25K === True);
- 
- 	then(startWorkout);
- 
- Console.log("Time to start walking to the Gym");
- 
- 	if(walk + gym);
- 	while(warmup);
- 
- then( console.log("RUN!!!"));
- 
- var handleData = function(json) {
-	for (var i = 0; <json.fitness apps.lenghh; i++) {
-	var vApp = json.fitness apps[i];
-	console.log ("Name:" + " Developer.Dev,:" + ",cost.cost" +, );
-	};
+if(coldRun === true){
+	console.log ("I have to Layer Up!!! ");{
 	
+}	if (body is warming as I run === true);
+		
+		console.log ("I'll keep running!!!");
+		} else {
+		console.log ("I's way to cold to run outside");
+		};
+} else {
+	console.log ("Looks like it's indoor running today.");
 
 };
 
-handleData(json2);
+// my weightloss starts with a daily vitamin.
+var pillsLeftInBottle = 100;
 
+while (pillsLeftInBottle > 0){
+	console.log (pillsLeftInBottle +  " 'one-A-day' Vitamins left in the bottle.");
+	
+	pillsLeftInBottle--;
+};
+
+console.log (" Time to make a vitamin run!!!");
+
+// I listed the 3 main, mojor workouts I enjoy doing.  If you don't enjoy a workout, you won't do it.
+
+var atwo = ["Upper Body", "Core", "Lower Body"];
+	atwo[0] = "Jillian's Biggest Looser Last chance workout DVD"
+	atwo[1] = "Jillian's 6 weeks to six pack DVD"
+	atwo[2] = "RUN!!!";
+console.log(atwo);
+
+//  The 4 major locations of my workouts.  I did have some trouble here, and understanding. 
+var workoutLocations = [ "Gym", "Living Room", "Wii-fit", "Road Bike"],
+	minsPerWorkout = [60, 20, 30, 120];
+for(var locationNumber = 0; locationNumber < workoutLocation.length; locationNumber++){
+	var workoutLocation = workoutLocations[locationNumber],
+		minsPerLocation = minsPerLocation[locationNumber];
+		console.log (" On my way to work out at the" + workoutLocation + "for" 
+					+ "minsPerLocation" + "minutes.");
+	
+	for (var minutes = 0; minutes < minPerLocation; minutes+= 10) {
+		var mintesRemain = minsPerLocation - minutes;
+		console.log(minutes + "Workout Complete," + minutesRemain + "to go"); 	
+	
+	}
+	console.log ("My workout at the " + workOutLocation + ".");
+ } 
  
+ // for times per locations
+
+
+// my argument code.  I became very lost here.
+var workoutLocations = [ "Gym", "Living Room", "Wii-fit", "Road Bike"],
+	minsPerWorkout = [60, 20, 30, 120];
+	
+var workoutAtOneLocation = function (locationNumber) {}
+	
+for(var locationNumber = 0; locationNumber < workoutLocation.length; locationNumber++){
+	var workoutLocation = workoutLocations[locationNumber],
+		minsPerLocation = minsPerLocation[locationNumber];
+		console.log (" On my way to work out at the" + workoutLocation + "for" 
+					+ "minsPerLocation" + "minutes.");
+	
+	for (var minutes = 0; minutes < minPerLocation; minutes+= 10) {
+		var mintesRemain = minsPerLocation - minutes;
+		console.log(minutes + "Workout Complete," + minutesRemain + "to go"); 	
+	
+	}
+	console.log ("My workout at the " + workOutLocation + ".");
+ };
  
- 
- 
-
-
-
-
-
-			 
+ var workoutAtAlllocations = function() {
+ 	for (var locationNumber = 0; locationNumber < workoutLocation.length; 		
+ 		locationNumber++) {
+ 		workoutAtOneLocation(locationNumber);
+ 		
+ 		
